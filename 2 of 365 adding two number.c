fd256f12f22b0949b2975578e233f585cb485e43
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-15 20:05:30 +0545

    Add files via upload

---
+++ 2 of 365 adding two number.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int num1, num2;
+	
+	printf("enter 1st number: ");
+	scanf("%d",&num1);
+	
+	printf("enter 2nd number: ");
+	scanf("%d",&num2);
+	
+	int sum = num1 + num2;
+	
+	printf("sum of %d and %d is %d",num1, num2, sum);
+	return 0;
+}
