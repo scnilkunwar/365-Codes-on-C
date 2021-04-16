7094da54c5d5bd255d5b435aed2332e5618df236
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-16 17:22:05 +0545

    Add files via upload

---
+++ 3 of 365 even or odd.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int num;
+	
+	printf("enter a number to check even or odd: ");
+	scanf("%d",&num);
+	
+	if(num % 2 == 0)
+	{
+		printf("%d is even",num);
+	}
+	
+	else
+	{
+		printf("%d is odd");
+	}
+	return 0;
+}
