de5996e2ffceb49787051d9534524f7b2e118e17
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-16 17:25:27 +0545

    Update 3 of 365 even or odd.c

--- 3 of 365 even or odd.c
+++ 3 of 365 even or odd.c
@@ -5,16 +5,16 @@ int main()
 	int num;
 	
 	printf("enter a number to check even or odd: ");
-	scanf("%d",&num);
+	scanf("%d", &num);
 	
 	if(num % 2 == 0)
 	{
-		printf("%d is even",num);
+		printf("%d is even", num);
 	}
 	
 	else
 	{
-		printf("%d is odd");
+		printf("%d is odd", num);
 	}
 	return 0;
 }
