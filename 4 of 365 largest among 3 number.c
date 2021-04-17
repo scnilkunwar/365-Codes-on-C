8152f950cbaaab1c66875740586a48aaba527be2
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-17 18:26:54 +0545

    Add files via upload

---
+++ 4 of 365 largest among 3 number.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	
+	printf("enter 1st number: ");
+	scanf("%d",&a);
+	printf("enter 2nd number: ");
+	scanf("%d",&b);
+	printf("enter 3rd number: ");
+	scanf("%d",&c);
+	
+	if(a > b && a > c)
+	{
+		printf("%d is largest",a);
+	}
+	
+	else if(b > a && b > c)
+	{
+		printf("%d is largest",b);
+	}
+	
+	else
+	{
+		printf("%d is largest",c);
+	}
+	return 0;
+}
