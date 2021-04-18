1ffa61ea5ae622d15f304189ee1807ab5ddae670
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-18 14:15:24 +0545

    Add files via upload

---
+++ 5 of 365 addition of digits of a number.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	long num;
+	
+	printf("enter a number: ");
+	scanf("%ld",&num);
+	
+	int temp = num, rem, sum = 0;
+	while(temp > 0)
+	{
+		rem = temp % 10;
+		sum += rem;
+		temp /= 10;
+	}
+	
+	printf("sum of digits of %ld is %d",num, sum);
+	
+	return 0;
+}
