0dd97633b3cbaf291a5c08c27e4ea93c282003dc
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-19 20:03:02 +0545

    Add files via upload

---
+++ 6 of 365 perfect square in limit.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int low, up;
+	
+	printf("enter lower limit: ");
+	scanf("%d",&low);
+	printf("enter upper limit: ");
+	scanf("%d",&up);
+	
+	int low_sqrt = ceil(sqrt(low));
+	int up_sqrt = floor(sqrt(up));
+	
+	for(int i=low_sqrt; i<=up_sqrt; i++)
+	{
+		printf("%d, ",i*i);
+	}
+	
+	printf("\b\b");
+	
+	return 0;
+}
