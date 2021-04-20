4901e4bdd0589ed577ab60b689a325f904838495
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-20 20:13:35 +0545

    Add files via upload

---
+++ 7 of 365 check prime number.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int isprime(int num)
+{
+	if(num < 2)
+		return 0;
+	
+	else
+	{
+		for(int i=2;i<=floor(sqrt(num));i++)
+		{
+			if(num % i == 0)
+				return 0;
+		}
+		return 1;
+	}
+}
+
+int main()
+{
+	int num;
+	printf("enter a number to check prime: ");
+	scanf("%d",&num);
+	
+	if(isprime(num) == 1)
+		printf("%d is prime number",num);
+		
+	else
+		printf("%d is not prime number",num);
+	
+	return 0;
+}
