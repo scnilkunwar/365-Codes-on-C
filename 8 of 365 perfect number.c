e7060ac150b5d1c3257f1701489fd555055f2ed6
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-21 19:43:33 +0545

    Add files via upload

---
+++ 8 of 365 perfect number.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+int isperfect(int num)
+{
+	int sum = 0;
+	for(int i=1;i<num;i++)
+	{
+		if(num % i == 0)
+			sum += i;
+	}
+	
+	if(sum == num)
+		return 1;
+	
+	else
+		return 0;
+}
+
+int main()
+{
+	int num;
+	printf("enter a number to check perfect: ");
+	scanf("%d",&num);
+	
+	if(isperfect(num))
+		printf("%d is perfect number",num);
+	
+	else
+		printf("%d is not perfect number",num);
+	
+	return 0;
+}
