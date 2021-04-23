f9373d86a7dfc3ae956ca33a396edc3431131197
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-23 19:25:56 +0545

    Add files via upload

---
+++ 10 of 365 factorial of a number.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+long long factorial(int num)
+{
+	if(num == 0)
+		return 1;
+	
+	else
+		return num*factorial(num-1);
+}
+
+int main()
+{
+	int n;
+	printf("enter a number: ");
+	scanf("%d",&n);
+	
+	printf("%d! = %lld", n, factorial(n));
+	return 0;
+}
