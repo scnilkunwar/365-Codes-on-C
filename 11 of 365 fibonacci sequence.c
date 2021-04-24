96043ddb2c0dd2d22a748939f6b758ab3394a2d7
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-24 20:46:28 +0545

    Add files via upload

---
+++ 11 of 365 fibonacci sequence.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int terms;
+	
+	printf("how many terms?\n");
+	scanf("%d",&terms);
+	
+	printf("fibonacci sequence up to %d terms:\n",terms);
+	for(int i=0;i<terms;i++)
+	{
+		long long static a = 0, b = 1, c;
+		printf("%lld, ",a);
+		
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	
+	printf("\b\b");
+	return 0;
+}
