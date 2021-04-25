c202ef252114709f8854b673747db6dff4564f95
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-25 19:23:55 +0545

    Add files via upload

---
+++ 12 of 365 armstrong number.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int intlen(long long num)
+{
+	int len = 0;
+	while(num > 0)
+	{
+		num /= 10;
+		len++;
+	}
+	
+	return len;
+}
+
+int isarmstrong(long long num)
+{
+	int len = intlen(num);
+	
+	long long sum = 0, temp = num;
+	while(temp > 0)
+	{
+		long long rem = temp % 10;
+		sum += pow(rem, len);
+		temp /= 10;
+	}
+	
+	if(sum == num)
+		return 1;
+	
+	else
+		return 0;
+}
+
+int main()
+{
+	long long n;
+	printf("enter a number: ");
+	scanf("%lld",&n);
+	
+	if(isarmstrong(n))
+		printf("%lld is armstrong number",n);
+	else
+		printf("%lld is not armstrong number",n);
+	
+	return 0;
+}
