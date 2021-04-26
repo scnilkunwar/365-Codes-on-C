a598d9a050622e78cfbb808ee47d69343da3fa17
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-26 19:24:37 +0545

    Add files via upload

---
+++ 13 of 365 HCF & LCM of Two Numbers.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int num1, num2;
+	printf("enter 1st number: ");
+	scanf("%d",&num1);
+	printf("enter 2nd number: ");
+	scanf("%d",&num2);
+	
+	int i;
+	for(i=num1;i>0;i--)
+	{
+		if(num1 % i == 0 && num2 % i == 0)
+		{
+			printf("HCF of %d and %d is %d",num1, num2,i);
+			break;
+		}
+	}
+	
+	printf("\nLCM of %d and %d is %d",num1,num2,(num1*num2)/i);
+	return 0;
+}
