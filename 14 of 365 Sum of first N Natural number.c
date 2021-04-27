d2ec4d26ddb1de3437fe2683a8abe4baf8b3625a
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-27 19:40:31 +0545

    Add files via upload

---
+++ 14 of 365 Sum of first N Natural number.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("how many numbers?\n");
+	scanf("%d",&n);
+	
+	int sum = (n*(n+1))/2;
+	printf("sum of natural numbers up to %d is %d",n,sum);
+	
+	return 0;
+}
