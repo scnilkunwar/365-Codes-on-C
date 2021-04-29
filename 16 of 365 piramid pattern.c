0a6d524b75cd28ade466b87705c285f7201501f8
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-29 20:27:54 +0545

    Add files via upload

---
+++ 16 of 365 piramid pattern.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int main()
+{
+	int n=7,i,j;
+	printf("\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=n;j>=i;j--)
+		{
+			printf("\t");
+		}
+		for(j=1;j<=(i*2-1);j++)
+		{
+			printf("*\t");
+		}
+		printf("\n");
+	}
+	return 0;
+}
