c648a23d0a59e349819a12c8e302c5cf5521ef61
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-04-30 19:23:58 +0545

    Add files via upload

---
+++ 17 of 365 flag of nepal.c
@@ -0,0 +1,63 @@
+﻿#include <stdio.h>
+void moon()
+{
+	printf("*   *         *   *\n");
+	printf("*   *    *    *     *\n");
+	printf("*    *  * *  *        *\n");
+	printf("*      * * *            *\n");
+}
+
+void sun()
+{
+	printf("*           * *             *\n");
+	printf("*         * * * *             *\n");
+	printf("*        * * * * *              *\n");
+	printf("*         * * * *                 *\n");
+	printf("*           * *                     *\n");
+}
+
+int main()
+{
+	for(int i=0;i<39;i++)
+	{
+		if(i == 9)
+		{
+			moon();
+			i += 3;
+		}
+		else if(i<17)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				if(i == 16)
+					printf("* ");
+				else if(j == 0 || j == i)
+					printf("* ");
+				else
+					printf("  ");
+			}
+			printf("\n");
+		}
+		
+		else if(i == 30)
+		{
+			sun();
+			i += 4;
+		}
+		
+		else
+		{
+			for(int j=0;j<=i-16;j++)
+			{
+				if(i == 38)
+					printf("* ");
+				else if(j == 0 || j == i-16)
+					printf("* ");
+				else
+					printf("  ");
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
