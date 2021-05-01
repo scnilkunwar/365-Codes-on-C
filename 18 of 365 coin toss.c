d47df7a214d593ed3317ed96267c1220b817574f
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-01 18:34:20 +0545

    Add files via upload

---
+++ 18 of 365 coin toss.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a;
+	char ch;
+	printf("head or tail(h/t)\n");
+	scanf("%c", &ch);
+	a = rand() % 2;
+	if(ch == 'h')
+	{
+		if(a == 0)
+			printf("you loss its tail");
+		
+		else if(a == 1)
+			printf("you win its head");
+	}
+	
+	else if(ch == 't')
+	{
+		if(a == 0)
+			printf("you win its tail");
+		
+		else if(a == 1)
+			printf("you loss its head");
+	}
+	
+	return 0;
+}
