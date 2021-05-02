94136194470fbbf3b27f538ba30eeca7506a1003
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-02 19:24:08 +0545

    Add files via upload

---
+++ 19 of 365 mximum and minimum of given numbers.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#define n 100
+
+int main()
+{
+	int num;
+	int arr[n];
+	printf("how many elements?\n");
+	scanf("%d",&num);
+	
+	printf("enter %d elements:\n",num);
+	for(int i=0;i<num;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	
+	int min = arr[0], max = arr[0];
+	for(int i=0;i<num;i++)
+	{
+		if(arr[i] < min)
+			min = arr[i];
+		
+		if(arr[i] > max)
+			max = arr[i];
+	}
+	
+	printf("maximum = %d\nminimum = %d",max, min);
+	return 0;
+}
