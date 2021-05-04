4adb6c248ba7c4d24a3986a0dd23378a485898bf
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-04 19:35:48 +0545

    Add files via upload

---
+++ 20 of 365 pythagorian triplets.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n, count=0;
+	printf("enter upper limit:");
+	scanf("%d",&n);
+	printf("\t\tp/b\t\tp/b\t\t h\n");
+	printf("\t---------------------------\n");
+	
+	for(int i=1;i<=n;i++)
+	{
+		long sq1 = i*i;
+		for(int j=i;j<=n;j++)
+		{
+			long sq2 = j*j;
+			long sq3 = sq1+sq2;
+			for(int k=i;k<=n;k++)
+			{
+				if(k*k == sq3)
+				{
+					printf("%9d%9d%9d\n",i, j, k);
+					count++;
+					break;
+				}
+			}
+		}
+	}
+	printf("there are total %d triplets",count);
+	
+	return 0;
+}
---
+++ 21 of 365 Amicable numbers.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+int sum_of_factors(int num)
+{
+	int sum = 0;
+	for(int i=1;i<=num/2;i++)
+	{
+		if(num % i == 0)
+			sum += i;
+	}
+	return sum;
+}
+
+int main()
+{
+	int num1, num2;
+	printf("enter 1st number: ");
+	scanf("%d",&num1);
+	
+	printf("enter 2nd number: ");
+	scanf("%d",&num2);
+	
+	if(num1 == sum_of_factors(num2))
+	{
+		if(num2 == sum_of_factors(num1))
+			printf("\n%d and %d are amicable numbers",num1, num2);
+		
+		else
+			printf("\n%d and %d are not amicable numbers",num1, num2);
+	}
+	
+	else
+		printf("\n%d and %d are not amicable numbers",num1, num2);
+	
+	return 0;
+}
