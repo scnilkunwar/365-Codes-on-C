64955f13418480e84b825c1771927cdccdb950f6
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-07 19:34:11 +0545

    Add files via upload

---
+++ 24 of 365 transformation of matrix.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#define n 10
+
+int main()
+{
+	int row, column, matrix[n][n];
+	printf("enter order of matrix: ");
+	scanf("%d %d",&row, &column);
+	for(int i=0;i<row;i++)
+	{
+		printf("enter %d elements of row%d: ",column, i+1);
+		for(int j=0;j<column;j++)
+		{
+			scanf("%d",&matrix[i][j]);
+		}
+	}
+	printf("\nmatrix\n");
+	for(int i=0;i<row;i++)
+	{
+		printf(" [");
+		for(int j=0;j<column;j++)
+		{
+			printf("%4d",matrix[i][j]);
+		}
+			printf("  ]\n");
+	}
+	printf("\ntransformation\n");
+	for(int i=0;i<column;i++)
+	{
+		printf(" [");
+		for(int j=0;j<row;j++)
+		{
+			printf("%4d",matrix[j][i]);
+		}
+			printf("  ]\n");
+	}
+	return 0;
+}
