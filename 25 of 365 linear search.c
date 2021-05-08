b9bdb9c32c0481546ee6cc82a8bdf42d3825b8a9
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-08 19:45:08 +0545

    Add files via upload

---
+++ 25 of 365 linear search.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int arr[] = {1,5,8,4,2,7,3}, n;
+	printf("enter element to search: ");
+	scanf("%d",&n);
+	int len = sizeof(arr)/sizeof(int), i;
+	for(i=0;i<len;i++)
+	{
+		if(arr[i] == n)
+		{
+			printf("%d is found at position %d",n, i+1);
+			break;
+		}
+	}
+	if(i == len)
+		printf("%d is not found in list",n);
+	return 0;
+}
