89c8ff2f0dcc0c763afbadac039cfa31dca99d8b
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-10 19:11:06 +0545

    Add files via upload

---
+++ 27 of 365 area of circle.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+	float r;
+	printf("enter radius of circle: ");
+	scanf("%f",&r);
+	
+	float area = PI*r*r;
+	printf("area of circle = %f",area);
+	return 0;
+}
