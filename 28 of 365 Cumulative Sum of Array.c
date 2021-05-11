4953994a1f483c5836c5f8ada6f4bedd032c6ce5
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-11 19:19:37 +0545

    Add files via upload

---
+++ 28 of 365 Cumulative Sum of Array.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+void print_arr(char topic[], int arr[], int len)
+{
+	puts(topic);
+	for(int i=0;i<len;i++)
+		printf("%d  ",arr[i]);
+	printf("\n");
+}
+
+int main()
+{
+	int arr[] = {5,7,3,9,1,7,4,6,5};
+	int len = sizeof(arr)/sizeof(int);
+	print_arr("original array:",arr, len);
+	
+	for(int i=len-1;i>=0;i--)
+	{
+		for(int j=i-1;j>=0;j--)
+		{
+			arr[i] += arr[j];
+		}
+	}
+	
+	print_arr("cumulative sum:", arr, len);
+	return 0;
+}
