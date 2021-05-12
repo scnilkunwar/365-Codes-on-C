9ca521b9bbba099326d7051d8d0bee2446e9981c
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-12 19:36:16 +0545

    Add files via upload

---
+++ 29 of 365 Permutation and Combination.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+
+long long factorial(int num)
+{
+	if(num == 0)
+		return 1;
+	
+	else
+		return num*factorial(num-1);
+}
+
+int main()
+{
+	int a, b;
+	char ch;
+	printf("enter 1st number: ");
+	scanf("%d",&a);
+	printf("enter 2nd number: ");
+	scanf("%d",&b);
+	
+	again:
+	printf("\nchoose option\npermutation/combination(p/c): ");
+	scanf("%c",&ch);
+	long long clcn;
+	switch(ch)
+	{
+		case 'p':
+			clcn = factorial(a)/factorial(a-b);
+			printf("\n%dP%d = %lld", a, b, clcn);
+		break;
+		
+		case 'c':
+			clcn = factorial(a)/(factorial(a-b)*factorial(b));
+			printf("\n%dC%d = %lld", a, b, clcn);	
+		break;
+		
+		default:
+			printf("\ninvalid option!!");
+			goto again;
+		break;
+	}
+	return 0;
+}
