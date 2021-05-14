29684172c93d7afbdbe8997bc3923afc251330ba
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-14 19:52:24 +0545

    Add files via upload

---
+++ 31 of 365 sum of Arithmetic series.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a, d, n;
+    
+    printf("Enter first term of A.P.: ");
+    scanf("%d",&a);
+    printf("Enter common difference of A.P.: ");
+    scanf("%d",&d);
+    printf("Enter total numbers in A.P.: ");
+    scanf("%d",&n);
+    
+    int sum = (n*(2*a+(n-1)*d))/2;
+    printf("Sum of A.P is: %d",sum);
+  
+    return 0;
+}
