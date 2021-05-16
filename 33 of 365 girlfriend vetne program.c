f0cb3aa2b6b131222d4bea70658f5ed1ce1cb08e
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-16 18:42:51 +0545

    Add files via upload

---
+++ 33 of 365 girlfriend vetne program.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char str1[] = "this string contain girlfriend";
+	char str2[] = "this string doesnt";
+	char *word;
+	
+	//for string1
+	word = strstr(str1, "girlfriend");
+	if(word != NULL)
+	{
+		int pos = word - str1 + 1;
+		printf("\"girlfriend\" found at pos %d in string1\n",pos);
+	}
+	else
+		printf("no \"girlfriend\" in string1\n");
+	
+	printf("\n");
+	//for string2
+	word = strstr(str2, "girlfriend");
+	if(word != NULL)
+	{
+		int pos = word - str1 + 1;
+		printf("\"girlfriend\" found at pos %d in string2\n",pos);
+	}
+	else
+		printf("no \"girlfriend\" in string2\n");
+	
+	return 0;
+}
