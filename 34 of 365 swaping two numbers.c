3bb178aca441581f8d84e65578fe9934fb9d379f
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-17 19:24:49 +0545

    Add files via upload

---
+++ 34 of 365 swaping two numbers.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main()
+{
+	int x = 17, y = 23;
+	printf("numbers before swapping:\nx = %d, y = %d\n", x, y);
+	
+	swap(&x, &y);
+	
+	printf("\nnumbers after swapping:\nx = %d, y = %d", x, y);
+	return 0;
+}
