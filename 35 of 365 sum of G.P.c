82454fc7add214a481dd0ee833ae6a850f3393ae
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-18 19:30:42 +0545

    Add files via upload

---
+++ 35 of 365 sum of G.P.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a, n;
+	float r;
+	printf("Enter 1st term of G.P.: ");
+	scanf("%d",&a);
+	printf("Enter total no of term of G.P.: ");
+	scanf("%d",&n);
+	printf("Enter common ratio of G.P.: ");
+	scanf("%f",&r);
+	
+	float sum = 0;
+	for(int i=0;i<n;i++)
+	{
+		float tn = a*pow(r, i);
+		sum += tn;
+	}
+	
+	printf("sum of G.P. is: %.3f",sum);
+	return 0;
+}
