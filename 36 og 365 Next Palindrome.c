1268bc414ccc04d84430ad104e108e2f8c91fda1
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-19 19:52:40 +0545

    Add files via upload

---
+++ 36 og 365 Next Palindrome.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+int check_palindrome(long long num)
+{
+	long long temp = num, rem, rev = 0;
+	while(temp > 0)
+	{
+		rem = temp % 10;
+		rev = rev*10 + rem;
+		temp /= 10;
+	}
+	if(num == rev)
+		return 1;
+	else
+		return 0;
+}
+
+int main()
+{
+	long long num;
+	printf("\n\nEnter a num: ");
+	scanf("%lld",&num);
+	
+	long long i = num;
+	while(1)
+	{
+		if(check_palindrome(i))
+		{
+			printf("Next palindome is: %lld\n",i);
+			break;
+		}
+		i++;
+	}
+	return 0;
+}
