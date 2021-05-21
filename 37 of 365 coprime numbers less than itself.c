2c7e16e351e32120e7620818a8baeeb07bc0eb5a
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-21 17:48:17 +0545

    Add files via upload

---
+++ 37 of 365 coprime numbers less than itself.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+int HCF(int a, int b)
+{
+	for(int i=a;i>0;i--)
+	{
+		if(a%i == 0 && b%i == 0)
+			return i;
+	}
+}
+
+int main()
+{
+	int x;
+	printf("Enter a number: ");
+	scanf("%d",&x);
+	
+	int count = 0;
+	for(int i=x-1;i>0;i--)
+	{
+		if(HCF(x, i) == 1)
+		{
+			printf("%d ",i);
+			count++;
+		}
+	}
+	printf("\nThere are %d coprimes of %d less than %d",count, x, x);
+	return 0;
+}
---
+++ 38 of 365 average of given numbers.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+float average(int arr[], len)
+{
+	int sum = 0;
+	for(int i=0;i<len;i++)
+	{
+		sum += arr[i];
+	}
+	
+	return (float) sum / len;
+}
+
+int main()
+{
+	int n;
+	printf("How many numbers?\n");
+	scanf("%d",&n);
+	
+	printf("Enter %d numbers:\n",n);
+	int *arr = (int *)calloc(n, sizeof(int));
+	
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",arr+i);
+	}
+	
+	printf("Average is: %.2f",average(arr, n));
+	return 0;
+}
