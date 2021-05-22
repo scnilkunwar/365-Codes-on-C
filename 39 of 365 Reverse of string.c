2ab2cba9f331c1fb0a8792683b937c31f84de838
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-22 20:00:46 +0545

    Add files via upload

---
+++ 39 of 365 Reverse of string.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *revstr(char str[])
+{
+	int len = strlen(str);
+	char *temp = (char *)calloc(len, sizeof(char));
+	
+	for(int i=len-1;i>=0;i--)
+	{
+		temp[len-i-1] = str[i];
+	}
+	return temp;
+	
+}
+
+int main()
+{
+	char my_str[] = "This is string";
+	char *ch = revstr(my_str);
+	printf("Original string:\n%s\n",my_str);
+	printf("\nReversed string:\n%s",ch);
+	return 0;
+}
