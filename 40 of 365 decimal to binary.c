dc542fa7ede7428abe597e6f4889c08aca8e3bce
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-23 19:40:17 +0545

    Add files via upload

---
+++ 40 of 365 decimal to binary.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+long long binary(int dec)
+{
+	if (dec == 0)
+		return 0;
+    else
+    	return (dec % 2) + 10 * binary(dec / 2);
+}
+     
+int main()
+{
+	int num;
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+    printf("The binary equivalent: %lld\n",binary(num));
+    return 0;
+}
