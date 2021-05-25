f183bb39300de71befd8a35ce80d6b657dc6489e
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-25 18:16:13 +0545

    Add files via upload

---
+++ 42 of 365 Palindrome String Check.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *revstr(char str[])
+{
+	int len = strlen(str);
+	char *temp = (char *)calloc(len, sizeof(char));
+	
+	for(int i=len-1;i>=0;i--)
+	{
+		temp[len-i-1] = str[i];
+	}
+	return temp;
+}
+
+int main()
+{
+	char str1[50];
+	printf("Enter a string: ");
+	gets(str1);
+	
+	char *str2 = revstr(str1);
+	if(strcmp(str1, str2) == 0)
+		printf("'%s' is palindrome",str1);
+	else
+		printf("'%s' is not palindrome",str1);
+	return 0;
+}
