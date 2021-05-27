aca18029bbf3f790b1ed94bd18dd3bf3da27f100
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-27 19:44:42 +0545

    Add files via upload

---
+++ 44 of 365 String splitting.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void strplit(char m_str[], char part[][20])
+{
+	int i = 0;
+	char *piece;
+	piece = strtok(m_str, " ");
+	while(piece != NULL)
+	{
+		strcpy(part[i++], piece);
+		piece = strtok(NULL, " ");
+	}
+}
+
+int main()
+{
+	char str[] = "i love c but i hate she", part[10][20];
+	
+	
+	strplit(str, part);
+	for(int i=0;i<10;i++)
+	{
+		printf("%s\n",part[i]);
+	}
+	return 0;
+}
