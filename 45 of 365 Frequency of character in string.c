625182179536e21f5ccfcd05436c78a13cc11c76
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-28 19:47:35 +0545

    Add files via upload

---
+++ 45 of 365 Frequency of character in string.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+void char_frequency(char str[], int freq[])
+{
+	for(int i=0;str[i] != '\0';i++)
+	{
+		freq[str[i]]++;
+	}
+}
+int main()
+{
+	char str[] = "this is a string!!!";
+	int freq[127] = {0};
+	char_frequency(str, freq);
+	
+	printf(" char  |  frequency\n");
+	printf("--------------------\n");
+	for(int i=0;i<127;i++)
+	{
+		if(freq[i] != 0)
+		{
+			if(i == 32)
+				continue;
+			printf(" '%c'   =   %d\n", i, freq[i]);
+		}
+	}
+	return 0;
+}
