67db426786207c5f159a76111832cd7d7afce30a
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-05-29 19:20:13 +0545

    Add files via upload

---
+++ 46 of 365 removing extra space from string.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void rmspace(char *str)
+{
+	int len = strlen(str);
+	for(int i=0;i<len;i++)
+	{
+		if(str[i] == ' ')
+		{
+			int j = i+1;
+			for(j=i+1;str[j]==' ';j++);
+			int k = j-i-1;
+			while(j <= len)
+			{
+				str[j-k] = str[j];
+				j++;
+			}
+			len = j;
+		}
+	}
+}
+
+int main()
+{
+	char my_str[] = "this      string   contains   extra   spaces";
+	
+	printf("With extra space:\n%s",my_str);
+	rmspace(my_str);
+	printf("\n\nWithout extra space:\n%s",my_str);
+	
+	return 0;
+}
