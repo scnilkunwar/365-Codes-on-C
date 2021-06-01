ff842f2cae2fe3069839fcf8c2809b08c8acd9de
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-06-01 19:18:17 +0545

    Add files via upload

---
+++ 48 of 365 Converting string into lowercase.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void strlwr(char str[])
+{
+	int len = strlen(str);
+	for(int i=0;i<len;i++)
+	{
+		if(str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+	}
+}
+
+int main()
+{
+	char str[] = "SUNIL kunwar)|&%]]`";
+	strlwr(str);
+	puts(str);
+	return 0;
+}
---
+++ 49 of 365 Converting string into uppercase.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void strupr(char str[])
+{
+	int len = strlen(str);
+	for(int i=0;i<len;i++)
+	{
+		if(str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+}
+
+int main()
+{
+	char str[] = "SUNIL kunwar";
+	strupr(str);
+	puts(str);
+	return 0;
+}
