b2215bdeab2ceadd893a2a50a5bfe064dfa174d0
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-06-06 20:26:39 +0545

    Add files via upload

---
+++ 53 of 365 Shuffiling Array.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void shuffle(int arr[], int len)
+{
+	for(int i=len-1;i>1;i--)
+	{
+		int pos = rand()%i;
+		int temp = arr[i];
+		arr[i] = arr[pos];
+		arr[pos] = temp;
+	}
+}
+
+void prin_arr(char topic[], int arr[], int len)
+{
+	puts(topic);
+	printf("[");
+	for(int i=0;i<len;i++)
+		printf("%d ",arr[i]);
+	printf("\b]");
+}
+
+int main()
+{
+	srand(time(NULL));
+	
+	int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	int len = sizeof(arr)/sizeof(arr[0]);
+	
+	prin_arr("original:", arr, len);
+	shuffle(arr, len);
+	prin_arr("\nshuffled:", arr, len);
+	return 0;
+}
---
+++ 54 of 365 Ternary Operator in C.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	
+	//	ternary operator
+	//	condition ? if_statement : else statement
+
+	a = 5, b = 10;
+	a < b ? printf("a is less\n") : printf("b is less\n");
+	
+	a = 10, b = 5;
+	a < b ? printf("a is less\n") : printf("b is less\n");
+	
+	return 0;
+}
