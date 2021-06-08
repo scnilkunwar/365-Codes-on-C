edb5c072c135abee2c743264869ab75608f48bc1
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-06-08 19:17:54 +0545

    Add files via upload

---
+++ 55 of 365 Random Integer in Interval.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int random_in_interval(int a, int b)
+{
+	return rand()%(b-a) + a;
+}
+
+int main()
+{
+	srand(time(NULL));
+	for(int i=0;i<10;i++)
+		printf("%d ",random_in_interval(50, 60));
+	
+	return 0;
+}
---
+++ 56 of 365 Prime sumation.c
@@ -0,0 +1,44 @@
+﻿
+// every even number can be represented as sum of two prime numbers except 2
+
+#include <stdio.h>
+#include <math.h>
+
+int isprime(int num)
+{
+	if(num<2)
+		return 0;
+	for(int i=2;i<=floor(sqrt(num));i++)
+	{
+		if(num%i == 0)
+		{
+			return 0;
+			break;
+		}	
+	}
+	return 1;
+}
+
+int main()
+{
+	int n;
+	printf("enter an even number: ");
+	scanf("%d",&n);
+		
+	if(n%2 != 0)
+	{
+		printf("%d is not even",n);
+		return 1;
+	}
+	
+	for(int i=2;i<n/2+1;i++)
+	{
+		if(isprime(i))
+		{
+			int b = n-i;
+			if(isprime(b))
+			    printf("%4d +%4d = %4d\n",i,b,n);
+		}
+	}
+	return 0;
+}
