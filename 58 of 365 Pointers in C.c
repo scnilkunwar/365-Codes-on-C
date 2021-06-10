1b927268fc80c721bd01a33d2034a55b39b6d91c
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-06-10 18:43:44 +0545

    Add files via upload

---
+++ 58 of 365 Pointers in C.c
@@ -0,0 +1,17 @@
+﻿
+//	58 of 365
+//	Pointers in C
+
+#include <stdio.h>
+
+int main()
+{
+	int var = 5;
+	int *ptr = &var;
+	
+	printf("adress of var from pointer = %x\n",ptr);
+	printf("adress of var from variable = %x\n",&var);
+	printf("value of var from pointer = %d\n",*ptr);
+	printf("value of var from variable = %d\n",var);
+	return 0;
+}
