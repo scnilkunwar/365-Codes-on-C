c2227e91d1806eb830ef3de23473675050d3662c
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-06-12 17:33:08 +0545

    Add files via upload

---
+++ 60 of 365 Function Pointer.c
@@ -0,0 +1,38 @@
+﻿
+//	Day 60 of 365
+//	Function Pointer
+
+#include <stdio.h>
+
+int my_func(int a, int b)
+{
+	if(a < b)
+		return (a *= 3, b += a);
+	else if(b < a)
+		return (b *= 3, a += b);
+	else
+		return 2*(a + b);
+}
+
+int main()
+{
+	//declearing function pointer and assigning
+	int (*func_ptr)(int, int);
+	func_ptr = my_func;
+	if(func_ptr == NULL)
+	{
+		printf("can't assign function to pointer");
+		return 1;
+	}
+	
+	int x = 6, y = 9;
+	int result;
+	
+	result = my_func(x, y);
+	printf("From function: %d\n",result);
+	
+	result = func_ptr(x, y);
+	printf("From pointer: %d",result);
+	
+	return 0;
+}
