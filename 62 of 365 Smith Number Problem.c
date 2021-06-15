46da972b1a738548adf5df15d2ebba3488786edd
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-06-15 17:17:40 +0545

    Add files via upload

---
+++ 62 of 365 Smith Number Problem.c
@@ -0,0 +1,68 @@
+﻿
+//	Day 62 of 365
+//	Smith Number Problem
+
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int sum_digit(long num)
+{
+	int sum = 0;
+	while(num > 0)
+	{
+		int rem = num % 10;
+		sum += rem;
+		num /= 10;
+	}
+	
+	return sum;
+}
+
+long prime_factor(long num)
+{
+	for(int i=2;i<=(int)sqrt(num);i++)
+	{
+		if(num % i == 0)
+			return i;
+	}
+	
+	return num;
+}
+
+bool is_smith(long num)
+{
+	int sum_num = sum_digit(num);
+	int sum_factor = 0;
+	
+	while(num > 1)
+	{
+		long factor = prime_factor(num);
+		sum_factor += sum_digit(factor);
+		num /= factor;
+	}
+	
+	if(sum_num == sum_factor)
+		return true;
+	
+	return false;
+}
+
+int main()
+{
+	long num;
+	printf("Enter a number: ");
+	scanf("%ld",&num);
+	
+	printf("The smallest smith number: ");
+	for(long i=num+1;;i++)
+	{
+		if(is_smith(i))
+		{
+			printf("%ld",i);
+			break;
+		}
+	}
+	
+	return 0;
+}
---
+++ 63 of 365 Guess Number Game.c
@@ -0,0 +1,97 @@
+﻿
+//	63 of 365
+//	Guess Number Game
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void title()
+{
+	printf("  ++-----------------------------------------------++\n");
+	printf("  ++-----------------------------------------------++\n");
+	printf("  ||\t  ooo    o   o   ooooo    oooo    oooo     ||\n");
+	printf("  ||\t o   o   o   o   o       o       o         ||\n");
+	printf("  ||\t o       o   o   o       o       o         ||\n");
+	printf("  ||\t o    o  o   o   oooo     ooo     ooo      ||\n");
+	printf("  ||\t  ooo o  o   o   o           o       o     ||\n");
+	printf("  ||\t      o  o   o   o           o       o     ||\n");
+	printf("  ||\t      o   ooo    ooooo   oooo    oooo      ||\n");
+	printf("  ||\t                                           ||\n");
+	printf("  ||\t\t Guess a Number Between 0-99           ||\n");
+	printf("  ++-----------------------------------------------++\n");
+	printf("  ++-----------------------------------------------++\n");
+	
+}
+
+void loss()
+{
+	printf("  ++-----------------------------------------------++\n");
+	printf("  ++-----------------------------------------------++\n");
+	printf("  ||\t o         oooo      oooo     oooo         ||\n");
+	printf("  ||\t o        o    o    o        o             ||\n");
+	printf("  ||\t o        o    o    o        o             ||\n");
+	printf("  ||\t o        o    o      ooo      ooo         ||\n");
+	printf("  ||\t o        o    o         o        o        ||\n");
+	printf("  ||\t o        o    o         o        o        ||\n");
+	printf("  ||\t ooooo     oooo      oooo     oooo         ||\n");
+	printf("  ||\t                                           ||\n");
+	printf("  ||\t\t\t\t  Try Again !!!                ||\n");
+	printf("  ++-----------------------------------------------++\n");
+	printf("  ++-----------------------------------------------++\n");
+	
+}
+
+void win()
+{
+	printf("  ++-----------------------------------------------++\n");
+	printf("  ++-----------------------------------------------++\n");
+	printf("  ||   o               o    ooooooo    oo      o   ||\n");
+	printf("  ||   o               o       o       o o     o   ||\n");
+	printf("  ||    o             o        o       o  o    o   ||\n");
+	printf("  ||     o     o     o         o       o   o   o   ||\n");
+	printf("  ||      o   o o   o          o       o    o  o   ||\n");
+	printf("  ||       o o   o o           o       o     o o   ||\n");
+	printf("  ||        o     o         ooooooo    o      oo   ||\n");
+	printf("  ||\t                                           ||\n");
+	printf("  ||\t\t\t  Congratulations !!!              ||\n");
+	printf("  ++-----------------------------------------------++\n");
+	printf("  ++-----------------------------------------------++\n");
+	
+}
+
+int main()
+{
+	srand(time(NULL));
+	int num, guess, i;
+	num = rand()%100;
+	
+	title();
+	
+	for(i=5;i>0;i--)
+	{
+		printf("\t%d guess ramaining!!\n",i);
+		printf("\tGuess: ");
+		scanf("%d",&guess);
+		
+		if(guess > num)
+			printf("\n\tguess is high!!\n\n");
+		
+		else if(guess < num)
+			printf("\n\tguess is low!!\n\n");
+		
+		else
+		{
+			printf("\n\tyou guessed it!!\n");
+			win();
+			break;
+		}
+	}
+	if(i == 0)
+	{
+		printf("\tNumber was %d\n",num);
+		loss();
+	}
+	
+	return 0;
+}
