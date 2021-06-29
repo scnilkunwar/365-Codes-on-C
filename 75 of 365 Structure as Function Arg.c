bd6f7514febe174e95cecce06d8e11b8cb75fbb6
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-06-29 18:28:45 +0545

    Add files via upload

---
+++ 75 of 365 Structure as Function Arg.c
@@ -0,0 +1,55 @@
+﻿
+//    Day 75 of 365
+//    Structure as Function Arg
+
+#include <stdio.h>
+
+typedef struct
+{
+	char *className;
+	int totalStudent;
+	int noOfBoy;
+	int noOfGirl;
+} class;
+
+void updateClassDetails(class *loc, class newDetail)
+{
+	loc ->className = newDetail.className;
+	loc ->totalStudent = newDetail.totalStudent;
+	loc ->noOfBoy = newDetail.noOfBoy;
+	loc ->noOfGirl = newDetail.totalStudent - newDetail.noOfBoy;
+}
+
+void printStructure(char *topic, class loc)
+{
+	printf("\n%s:\n",topic);
+    printf("Class Name: %s\n",loc.className);
+    printf("Total Students: %d\n",loc.totalStudent);
+    printf("No of Boy: %d\n",loc.noOfBoy);
+    printf("No of Girl: %d\n",loc.noOfGirl);
+}
+int main()
+{
+    class class9 = {"Class 09 Sec 'A'", 80, 45, 35};
+    
+    printStructure("Class Details", class9);
+   
+    class newClass = {"Class 10 Sec 'C'", 75, 37};
+    updateClassDetails(&class9, newClass);
+    
+    printStructure("New Class Details", class9);
+    return 0;
+}
+
+
+Class Details:
+Class Name: Class 09 Sec 'A'
+Total Students: 80
+No of Boy: 45
+No of Girl: 35
+
+New Class Details:
+Class Name: Class 10 Sec 'C'
+Total Students: 75
+No of Boy: 37
+No of Girl: 38
---
+++ 76 of 365 Adding Two Number Without '+' Sign.c
@@ -0,0 +1,28 @@
+﻿
+//	Day 76 of 365
+//	Adding Two Number Without '+' Sign
+
+#include <stdio.h>
+
+int addWithoutPlus(int num1, int num2)
+{
+	int sum = num1 - (-num2);
+	return sum;
+}
+
+int main()
+{
+	
+	int testCase[4][2] = {{79, 29},
+						  {-61, 37},
+						  {47, -31},
+						  {-61, -73}};
+	for(int i=0;i<4;i++)
+		printf("%d and %d = %d\n",testCase[i][0], testCase[i][1], addWithoutPlus(testCase[i][0], testCase[i][1]));
+	
+	return 0;
+}
+79 and 29 = 108
+-61 and 37 = -24
+47 and -31 = 16
+-61 and -73 = -134
