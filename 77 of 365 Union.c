3ba2be3b66fa8a89381b5aa8c79f373c58cd3de7
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-06-30 19:48:31 +0545

    Add files via upload

---
+++ 77 of 365 Union.c
@@ -0,0 +1,26 @@
+﻿
+//	Day 77 of 365
+//	Union
+
+#include <stdio.h>
+#include <string.h>
+
+typedef union var
+{
+	int i_num;
+	float f_num;
+	char str[20];
+} var;
+
+
+int main()
+{
+	var v1;
+	
+	v1.f_num = 3.14;
+	v1.i_num = 2;
+	strcpy(v1.str, "union example");
+	
+	printf("%s",v1.str);
+	return 0;
+}
---
+++ 78 of 365 Time Period of Pendulum.c
@@ -0,0 +1,26 @@
+﻿
+//	Day 78 of 365
+//	Time period of Pendulum
+
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+#define g 9.81
+
+float timePeriod(float length)
+{
+	float temp = sqrtf(length / g);
+	return 2*PI*temp;
+}
+
+int main()
+{
+	float testCase[] = {11, 29, 3, 79, 73};
+	
+	for(int i=0;i<5;i++)
+	{
+		printf("Length = %.2f m, ",testCase[i]);
+		printf("Time Period = %.2f sec\n",timePeriod(testCase[i]));
+	}
+	return 0;
+}
